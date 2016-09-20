ae13846702f0f11280210f33871be30e7a472cf9
khanhndk <khanhndk@hcmup.edu.vn>
2016-09-20 15:45:46 +1000

    Change learning rate to mistake rate

--- AOSVM/report.h
+++ AOSVM/report.h
@@ -13,12 +13,12 @@ public:
 	double train_time;
 	double predict_time;
 
-	std::vector<double> learning_rate;
-	std::vector<double>* learning_class_rate; //detail for each class
+	std::vector<double> mistake_rate;
+	std::vector<double>* mistake_class_rate; //detail for each class
 
 	report()
 	{
-		learning_class_rate = NULL;
+		mistake_class_rate = NULL;
 	}
 
 	void start();
@@ -35,35 +35,35 @@ public:
 		std::ofstream fp(filename, std::ofstream::out | std::ofstream::app);
 		if (strlen(param.testid) > 0)
 			fp << "testid: " << param.testid << "\tcrossid: " << param.crossid << "\trunid: " << param.runid << "\t";
-		fp << "learning_rate: " << learning_rate[learning_rate.size() - 1] << "\t";
+		fp << "mistake_rate: " << mistake_rate[mistake_rate.size() - 1] << "\t";
 
-		if (learning_class_rate != NULL)
+		if (mistake_class_rate != NULL)
 		{
 			for (int c = 0; c < n_class; c++)
-				fp << "learning_rate" + std::to_string(c) + ": " << learning_class_rate[c][learning_class_rate[c].size() - 1] << "\t";
+				fp << "mistake_rate" + std::to_string(c) + ": " << mistake_class_rate[c][mistake_class_rate[c].size() - 1] << "\t";
 
 			if (n_class == 2)
 			{
-				mydouble neg_rate = learning_class_rate[0][learning_class_rate[0].size() - 1];
-				mydouble pos_rate = learning_class_rate[1][learning_class_rate[1].size() - 1];
-				fp << "learning_rate_oc: " << (pos_rate + neg_rate) / 2 << "\t";
+				mydouble neg_rate = mistake_class_rate[0][mistake_class_rate[0].size() - 1];
+				mydouble pos_rate = mistake_class_rate[1][mistake_class_rate[1].size() - 1];
+				fp << "mistake_rate_oc: " << (pos_rate + neg_rate) / 2 << "\t";
 			}
 		}
 		fp << "train: " << train_time << "\t";
 
 		fp << param.arg;
 
-		fp << "learning_rate_detail: ";
-		for (int m = 0; m < learning_rate.size(); m++)
-			fp << learning_rate[m] << "|";
+		fp << "mistake_rate_detail: ";
+		for (int m = 0; m < mistake_rate.size(); m++)
+			fp << mistake_rate[m] << "|";
 		fp << "\t";
-		if (learning_class_rate != NULL)
+		if (mistake_class_rate != NULL)
 		{
 			for (int c = 0; c < n_class; c++)
 			{
-				fp << "learning_rate" + std::to_string(c) + ": ";
-				for (int m = 0; m < learning_class_rate[c].size(); m++)
-					fp << learning_class_rate[c][m] << "|";
+				fp << "mistake_rate" + std::to_string(c) + ": ";
+				for (int m = 0; m < mistake_class_rate[c].size(); m++)
+					fp << mistake_class_rate[c][m] << "|";
 				fp << "\t";
 			}
 		}
